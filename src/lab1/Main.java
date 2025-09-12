0b35d4d463167fb6140a06d2ee74a07279af7031
theduth <parkerwduthie@gmail.com>
2025-09-12 17:44:34 -0400

    added main class for doFizzBuzz

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = new String[100];
+        FizzBuzz.main(arr);
+        FizzBuzz.doFizzBuzz(5);
+    }
+}
